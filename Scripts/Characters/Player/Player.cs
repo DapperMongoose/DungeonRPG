c0e54970255593a5505dd046bfa100596a5a7729
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-17 19:52:57 -0500

    completed moving the player lesson

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -2,11 +2,16 @@ using Godot;
 using System;
 
 public partial class Player : CharacterBody3D {
+    private Vector2 _direction = new();
     public override void _PhysicsProcess(double delta) {
-        GD.Print("Player physics process!");
+        Velocity = new Vector3(_direction.X, 0, _direction.Y);
+        Velocity *= 5;
+
+        MoveAndSlide();
     }
     
     public override void _Input(InputEvent @event) {
-        GD.Print("Player input!");
+        _direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+        
     }
 }
