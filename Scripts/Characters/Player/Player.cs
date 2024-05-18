ef72ccfe1eb4679c3815d35f95e173e42192ec73
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-17 19:41:09 -0500

    completed overriding methods lesson

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -2,4 +2,11 @@ using Godot;
 using System;
 
 public partial class Player : CharacterBody3D {
+    public override void _PhysicsProcess(double delta) {
+        GD.Print("Player physics process!");
+    }
+    
+    public override void _Input(InputEvent @event) {
+        GD.Print("Player input!");
+    }
 }
