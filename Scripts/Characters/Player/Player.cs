5a5ee05fc5a191cd998f59d41051deb118e1425a
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-17 20:24:47 -0500

    completed flipping the player lesson

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -6,7 +6,7 @@ public partial class Player : CharacterBody3D
 {
     [ExportGroup("Required Nodes")]
     [Export] private AnimationPlayer _animPlayerNode;
-    [Export] private Sprite3D _sprite;
+    [Export] private Sprite3D _spriteNode;
     
     private Vector2 _direction;
 
@@ -17,8 +17,9 @@ public partial class Player : CharacterBody3D
     public override void _PhysicsProcess(double delta) {
         Velocity = new Vector3(_direction.X, 0, _direction.Y);
         Velocity *= 5;
-
+        
         MoveAndSlide();
+        Flip();
     }
     
     public override void _Input(InputEvent @event) {
@@ -35,5 +36,16 @@ public partial class Player : CharacterBody3D
         else {
             _animPlayerNode.Play(GameConstants.AnimMove);
         }
+    }
+
+    private void Flip() {
+        bool isNotMovingHorizontally = Velocity.X == 0;
+
+        if (isNotMovingHorizontally) {
+            return;
+        }
+        
+        bool isMovingLeft = Velocity.X < 0;
+        _spriteNode.FlipH = isMovingLeft;
     }
 }
