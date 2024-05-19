a464a6628d5ab987e3926a734a7c131488beb6f6
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-19 18:52:33 -0500

    completed finalizing the dash state lesson

--- Scripts/Characters/Player/PlayerDashState.cs
+++ Scripts/Characters/Player/PlayerDashState.cs
@@ -5,6 +5,7 @@ using System;
 public partial class PlayerDashState : Node {
     private Player _characterNode;
     [Export] private Timer _dashTimerNode;
+    [Export] private float _speed = 10;
 
     public override void _Ready() {
         _characterNode = GetOwner<Player>();
@@ -18,13 +19,26 @@ public partial class PlayerDashState : Node {
         if (what == 5001) {
             _characterNode.AnimPlayerNode.Play(GameConstants.AnimDash);
             SetPhysicsProcess(true);
+            _characterNode.Velocity = new Vector3(_characterNode.Direction.X, 0, _characterNode.Direction.Y);
+
+            if (_characterNode.Velocity == Vector3.Zero) {
+                _characterNode.Velocity = _characterNode.SpriteNode.FlipH ? Vector3.Left : Vector3.Right;
+            }
+            
+            _characterNode.Velocity *= _speed;
             _dashTimerNode.Start();
         } else if (what == 5002) {
             SetPhysicsProcess(false);
         }
     }
+    
+    public override void _PhysicsProcess(double delta) {
+        _characterNode.MoveAndSlide();
+        _characterNode.Flip();
+    }
 
     private void HandleDashTimeout() {
+        _characterNode.Velocity = Vector3.Zero;
         _characterNode.StateMachineNode.SwitchState<PlayerIdleState>();
     }
 }
