33e5317370ae98453d110a05da899d9f5d0f6c98
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-21 12:38:26 -0500

    completed pausing on patrols lesson

--- Scripts/Characters/Enemy/EnemyPatrolState.cs
+++ Scripts/Characters/Enemy/EnemyPatrolState.cs
@@ -1,7 +1,10 @@
 using Godot;
 using System;
+using System.Linq.Expressions;
 
 public partial class EnemyPatrolState : EnemyState {
+    [Export] private Timer _idleTimerNode;
+    [Export(PropertyHint.Range, "0, 20, 0.1")] private float _maxIdleTime = 4;
     private int _pointIndex = 0;
 
     protected override void EnterState() {
@@ -13,13 +16,28 @@ public partial class EnemyPatrolState : EnemyState {
         CharacterNode.AgentNode.TargetPosition = Destination;
 
         CharacterNode.AgentNode.NavigationFinished += HandleNavigationFinished;
+
+        _idleTimerNode.Timeout += HandleTimeout;
     }
 
     public override void _PhysicsProcess(double delta) {
+        if (!_idleTimerNode.IsStopped()) {
+            return;
+        }
         Move();
     }
     
     private void HandleNavigationFinished() {
+        CharacterNode.AnimPlayerNode.Play(GameConstants.AnimIdle);
+
+        RandomNumberGenerator rng = new();
+        _idleTimerNode.WaitTime = rng.RandfRange(0, _maxIdleTime);
+        _idleTimerNode.Start();
+    }
+    
+    
+    private void HandleTimeout() {
+        CharacterNode.AnimPlayerNode.Play(GameConstants.AnimMove);
         _pointIndex = Mathf.Wrap(_pointIndex + 1, 0, CharacterNode.PathNode.Curve.PointCount);
 
         Destination = GetPointGlobalPosition(_pointIndex);
