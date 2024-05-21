f702604b8c7d02b65ac3faf915192b60a26b46fc
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-21 16:03:43 -0500

    completed resetting the combo counter with a timer lesson

--- Scripts/Characters/Player/PlayerAttackState.cs
+++ Scripts/Characters/Player/PlayerAttackState.cs
@@ -2,16 +2,24 @@ using Godot;
 using System;
 
 public partial class PlayerAttackState : PlayerState {
+    [Export] private Timer _comboTimerNode;
+    
     private int _comboCounter = 1;
     private int _maxComboCount = 2;
 
+    public override void _Ready() {
+        base._Ready();
+        _comboTimerNode.Timeout += () => _comboCounter = 1;
+    }
+
     protected override void EnterState() {
-        CharacterNode.AnimPlayerNode.Play(GameConstants.AnimAttack + _comboCounter);
+        CharacterNode.AnimPlayerNode.Play(GameConstants.AnimAttack + _comboCounter, -1, 1.5f);
         CharacterNode.AnimPlayerNode.AnimationFinished += HandleAnimationFinished;
     }
 
     protected override void ExitState() {
         CharacterNode.AnimPlayerNode.AnimationFinished -= HandleAnimationFinished;
+        _comboTimerNode.Start();
     }
 
     private void HandleAnimationFinished(StringName animname) {
