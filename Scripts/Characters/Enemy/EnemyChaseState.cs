3c89f8eb406986e220b6d54dfabdac8c42d0175b
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-21 14:41:18 -0500

    completed chasing the player lesson

--- Scripts/Characters/Enemy/EnemyChaseState.cs
+++ Scripts/Characters/Enemy/EnemyChaseState.cs
@@ -1,9 +1,28 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class EnemyChaseState : EnemyState {
+    private CharacterBody3D _target;
+    [Export] private Timer _timerNode; 
+    
     protected override void EnterState() {
         CharacterNode.AnimPlayerNode.Play(GameConstants.AnimMove);
+        _target = CharacterNode.ChaseAreaNode.GetOverlappingBodies().First() as CharacterBody3D;
+        _timerNode.Timeout += HandleTimeout;
         GD.Print("chase state");
+    }
+
+    protected override void ExitState() {
+        _timerNode.Timeout -= HandleTimeout;
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        Move();
+    }
+    
+    private void HandleTimeout() {
+        Destination = _target.GlobalPosition;
+        CharacterNode.AgentNode.TargetPosition = Destination;
     }
 }
