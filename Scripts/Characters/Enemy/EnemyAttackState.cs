f57d0ea2d02666ef56f3442c23dbab19267721c4
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-21 19:48:27 -0500

    completed finishing the enemy attack state lesson

--- Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -9,15 +9,40 @@ public partial class EnemyAttackState : EnemyState {
         CharacterNode.AnimPlayerNode.Play(GameConstants.AnimAttack);
         Node3D target = CharacterNode.AttackAreaNode.GetOverlappingBodies().First();
         _targetPosition = target.GlobalPosition;
+        CharacterNode.AnimPlayerNode.AnimationFinished += HandleAnimationFinished;
     }
 
     protected override void ExitState() {
-        CharacterNode.ToggleHitBox(true);
+        CharacterNode.AnimPlayerNode.AnimationFinished -= HandleAnimationFinished;
     }
 
     private void PerformHit() {
         CharacterNode.ToggleHitBox(false);
         CharacterNode.HitBoxNode.GlobalPosition = _targetPosition;
+    }
+    
+    
+    private void HandleAnimationFinished(StringName animname) {
+        CharacterNode.ToggleHitBox(true);
+
+        Node3D target = CharacterNode.AttackAreaNode.GetOverlappingBodies().FirstOrDefault();
+
+        if (target == null) {
+            Node3D chaseTarget = CharacterNode.ChaseAreaNode.GetOverlappingBodies().FirstOrDefault();
+
+            if (chaseTarget == null) {
+                CharacterNode.StateMachineNode.SwitchState<EnemyReturnState>();
+                return;
+            }
+            
+            CharacterNode.StateMachineNode.SwitchState<EnemyChaseState>();
+            return;
+        }
         
+        Vector3 direction = CharacterNode.GlobalPosition.DirectionTo(_targetPosition);
+        CharacterNode.SpriteNode.FlipH = direction.X < 0;
+        
+        CharacterNode.AnimPlayerNode.Play(GameConstants.AnimAttack);
+        _targetPosition = target.GlobalPosition;
     }
 }
